d72e405cf7829a3e81b3f49b256a59b02cb76005
luzlozas <78167402+luzlozas@users.noreply.github.com>
2021-06-27 22:49:23 +0200

    Subida del proyecto de Recuperación

---
+++ src/es/edix/ed/recuperacion/Clase.java
@@ -0,0 +1,129 @@
+package es.edix.ed.recuperacion;
+import org.junit.platform.commons.util.*;
+
+import org.junit.platform.commons.util.StringUtils;
+
+
+/**
+ * Esta clase contiene dos métodos, Saludar y Matematicas. <br>
+ * <ul>
+ * <li>Saludar detecta el tipo de parámetro y, en función de esto, devuelve un valor u otro.<br>
+ * 
+ * Excepciones metodo Saludar
+ * <ol>
+ * <li> Si no consigue convertir el parámetro en Número devuelve una excepción NumberFormatException <br>
+ * </ol>
+ * <li>Matematicas realiza operaciones matemáticas sencillas como sumar, restar, multiplicar y dividir introduciendo 3 parámetros.<br>
+ * 
+ * Defaulter metodo Matematicas
+ * <ol>
+ * <li> Si la operación no está dentro de las posibilidades nos devuelve un valor combinado de los Números introducidos.<br>
+ * </ol>
+ * </ul>
+ * 
+ * @author luzlo
+ * @version 1.0
+ * @since June2021
+ * 
+ */
+
+
+
+public class Clase {
+
+	private static final String NFE = "Lo que me mandas es un numero";
+	private static final String BLANK = "Lo que me mandas está vacío";
+	
+/**
+
+ 	* Recibe un parámetro de tipo String, intenta convertirlo en tipo Número, si lo consigue nos devuelve distintos mensajes.<br>
+ 	* Sino lo consigue nos devuelve el saludo.
+ 	* 
+ 	*
+ 	* @param nombre tipo String. El nombre que quieres que salga en el saludo.
+ 	* @return  
+ 	* <ul>
+ 	* <li> Si consigue convertir el parámetro a Int devuelve el mensaje tipo String:<br>
+ 	* "Lo que me mandas es un numero"<br>
+ 	* 
+ 	* <li> Si el parámetro está vacío devuelve el mensaje tipo String:<br>
+ 	* 
+ 	* "Lo que me mandas está vacío"<br>
+ 	* 
+ 	* <li> Si no consigue convertirlo devuelve el saludo tipo String "Hola + parametro de entrada".<br>
+ 	* </ul>
+
+*/
+
+	public String saludar(String nombre){
+		try {
+			Integer.parseInt(nombre);
+			return NFE;
+		}catch (NumberFormatException e) {
+		}
+		if(StringUtils.isBlank(nombre)) {
+			return BLANK;
+		}
+		return "Hola " + nombre;
+	}
+	
+
+/**
+
+ 	* Recibe tres parámetros, dos de tipo Int y uno de tipo String. Los de tipo Int serán los números con los que queremos opoerar<br>
+ 	* El de tipo String será la operación que queremos realizar, sumar, restar, multiplicar o dividir.
+ 	* 
+ 	*
+
+ 	* @param x tipo Int. El número con el que quieres operar.
+ 	* @param y tipo Int. El número con el que quieres operar.
+ 	* @param op tipo String. La operación que quieres realizar. Puede ser sumar, restar, multiplicar o dividir. 
+ 	* 
+ 	* @return  
+ 	* 
+ 	*<ul>
+ 	* <li> op Sumar: <br>
+ 	* Devuelve dato tipo Int con el resultado de la suma de x + y.<br>
+ 	* 
+ 	* <li> op Restar:<br>
+ 	* 
+ 	* Devuelve dato tipo Int el resultado de la resta de x - y.<br>
+ 	* 
+ 	* <li> op Multiplicar:<br>
+ 	* 
+ 	* Devuelve dato tipo Int el resultado de la multiplicación de x * y.
+ 	* 
+ 	* <li> op Dividir: <br>
+ 	* 
+ 	* Devuelve dato tipo Int el resultado de la división de x / y. <br>
+ 	* 
+ 	* <li> op Otro dato: <br>
+ 	* 
+ 	* Devuelve un dato tipo double, primero convierte x e y en tipo String y tras esto le da el formato double con ambos números y un punto (.) entre ambos.<br>
+ 	* Por ejemplo 3.2. En el caso de que x sea 3 e y sea 2.
+ 	* 
+ 	* </ul>
+ 	* 
+
+*/
+	
+	public double matematicas(int x, int y, String op){
+		switch (op) {
+			case "sumar":
+				return x+y;
+			case "restar":
+				return x-y;
+			case "multiplicar":
+				return x*y;
+			case "dividir":
+				return x/y;
+			default:
+				return Double.valueOf(String.format("%1d.%1d", x, y));
+		}
+	}
+	
+	
+
+}
+
+
---
+++ src/es/edix/ed/recuperacion/TestClase.java
@@ -0,0 +1,121 @@
+package es.edix.ed.recuperacion;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestClase {
+	
+	Clase name = new Clase();
+
+	@Test
+	void TestAlDarleASaludarUnNumero() {
+		
+		String esperado = "Lo que me mandas es un numero";
+		String obtenido = name.saludar("20");
+		
+		
+		assertEquals(esperado,obtenido,"Debería devolver una excepción");
+	}
+	
+	@Test
+	void TestAlDarleASaludarUnString() {
+		
+		String esperado = "Hola Luz";
+		String obtenido = name.saludar("Luz");
+		
+		assertEquals(esperado,obtenido,"Debería devolver Hola Luz");
+		
+	}
+	
+	@Test
+	void TestAlDarleUnValorVacio() {
+		
+		String esperado = "Lo que me mandas está vacío";
+		String obtenido = name.saludar("");
+		
+		assertEquals(esperado,obtenido,"Debería devolver el mensaje Lo que me mandas es un vacío");
+	}
+	
+	@Test
+	
+	void TestAlDarleUnValorNuméricoYUnNombre() {
+		
+		String esperado = "Hola 12 Luz";
+		String obtenido = name.saludar("12 Luz");
+		
+		assertEquals(esperado,obtenido,"Debería devolver Hola 12 Luz");
+	}
+	
+	@Test
+	
+	void TestSuma() {
+		
+		int x = 1;
+		int y = 2;
+		String op = "sumar";
+		int esperado = 3;
+		
+		assertEquals(esperado, name.matematicas(x, y, op), "Debería devolver un 3");
+		
+	}
+	
+	@Test
+	
+	void TestResta() {
+		
+		int x = 3;
+		int y = 2;
+		String op = "restar";
+		int esperado = 1;
+		
+		assertEquals(esperado, name.matematicas(x, y, op), "Debería devolver un 1");
+		
+	}
+	
+	@Test
+	
+	void TestMultiplica() {
+		
+		int x = 3;
+		int y = 2;
+		String op = "multiplicar";
+		int esperado = 6;
+		
+		assertEquals(esperado, name.matematicas(x, y, op), "Debería devolver un 6");
+		
+	}
+	
+	@Test
+	
+	void TestDivide() {
+		
+		int x = 8;
+		int y = 4;
+		String op = "dividir";
+		int esperado = 2;
+		
+		assertEquals(esperado, name.matematicas(x, y, op), "Debería devolver un 2");
+	
+		
+	}
+	
+	@Test
+	
+	void TestSiNoLeDamosLasOperacionesEsperadas() {
+		
+		int x = 8;
+		int y = 4;
+		String op = "hola";
+		double esperado = 8.4;
+		
+		assertEquals(esperado, name.matematicas(x, y, op), "Debería devolver un 8.4");
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
